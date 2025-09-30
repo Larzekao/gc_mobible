91d4185955ebc2a974410ee794467a6c0e340c16
Javier Cruz <321javiercruz@gmail.com>
2025-09-30 01:26:32 -0400

    movil todo listo

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,6 +1,6 @@
 class Config {
   //static const String baseUrl = "http://127.0.0.1:8000/api";
-  static const String baseUrl = "http://192.168.1.104:8000/api";
-  //static const String baseUrl =
-  //   "https://condominium-api-staging.up.railway.app/api";
+  //static const String baseUrl = "http://192.168.1.104:8000/api";
+  static const String baseUrl =
+      "https://condominium-api-staging.up.railway.app/api";
 }
